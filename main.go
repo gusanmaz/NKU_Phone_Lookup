fdb95d338ceea609d663a8c0d2fec14fb560c8b2
Guvenc Usanmaz <gusanmaz@gmail.com>
2018-01-30 23:02:49 +0200

    Go fmt is run on first commit.

--- main.go
+++ main.go
@@ -3,21 +3,20 @@
 package main
 
 import (
-	"github.com/urfave/cli"
-	"os/exec"
 	"fmt"
-	"os"
-	"strings"
 	"github.com/anaskhan96/soup"
+	"github.com/urfave/cli"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 var cmdTmpl string = "curl -s 'http://rehber.nku.edu.tr/iptelefonlistesiarama.php' -H 'Host: rehber.nku.edu.tr' -H " +
 	"'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/55.0' -H 'Accept: */*' -H " +
-		"'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: http://rehber.nku.edu.tr/' " +
-			"-H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: " +
-				"XMLHttpRequest' -H 'Cookie: _ga=GA1.3.546134864.1513155902; _gid=GA1.3.111753471.1517302490' -H " +
-					"'Connection: keep-alive' --data 'birim=DEPARTMENT&ad=NAME'"
-
+	"'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: http://rehber.nku.edu.tr/' " +
+	"-H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: " +
+	"XMLHttpRequest' -H 'Cookie: _ga=GA1.3.546134864.1513155902; _gid=GA1.3.111753471.1517302490' -H " +
+	"'Connection: keep-alive' --data 'birim=DEPARTMENT&ad=NAME'"
 
 func main() {
 	app := cli.NewApp()
@@ -25,43 +24,43 @@ func main() {
 	app.Usage = "Look up phone numbers of NKU personnel by name or/and department by using appropriate flags"
 	app.Version = "0.1"
 	app.Author = "Guvenc Usanmaz"
-	app.Email  = "gusanmaz <ett> gM@|l C_0M"
+	app.Email = "gusanmaz <ett> gM@|l C_0M"
 
-	app.Flags = []cli.Flag {
+	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:        "name, n",
-			Value:       "",
-			Usage:       "Name of the personnel to query",
+			Name:  "name, n",
+			Value: "",
+			Usage: "Name of the personnel to query",
 		},
 		cli.StringFlag{
-			Name:        "department, d",
-			Value:       "",
-			Usage:       "Name of the department to query",
+			Name:  "department, d",
+			Value: "",
+			Usage: "Name of the department to query",
 		},
 	}
 
 	app.Action = func(c *cli.Context) error {
 		var name = c.String("name")
 		var department = c.String("department")
-		commandStr := strings.Replace(cmdTmpl, "DEPARTMENT", department,1)
-		commandStr = strings.Replace(commandStr, "NAME", name,1)
+		commandStr := strings.Replace(cmdTmpl, "DEPARTMENT", department, 1)
+		commandStr = strings.Replace(commandStr, "NAME", name, 1)
 
 		cmdOutByte, _ := exec.Command("/bin/sh", "-c", commandStr).Output()
 		cmdOutStr := string(cmdOutByte)
 
 		htmlDoc := soup.HTMLParse(cmdOutStr)
 		elems := htmlDoc.FindAll("td")
-		elemsLen  := len(elems)
+		elemsLen := len(elems)
 		rowCnt := (elemsLen / 4)
 
 		seperatorLen := 0
 		for id := 0; id < rowCnt; id++ {
-			nameLen 	  := len(elems[4 * id + 0].Text())
-			departmentLen := len(elems[4 * id + 1].Text())
-			if nameLen > seperatorLen{
+			nameLen := len(elems[4*id+0].Text())
+			departmentLen := len(elems[4*id+1].Text())
+			if nameLen > seperatorLen {
 				seperatorLen = nameLen
 			}
-			if departmentLen > seperatorLen{
+			if departmentLen > seperatorLen {
 				seperatorLen = departmentLen
 			}
 		}
@@ -69,11 +68,11 @@ func main() {
 		seperator := strings.Repeat("-", seperatorLen)
 
 		fmt.Println("NKU PHONE LOOKUP\n")
-		for id := 0; id < rowCnt; id++{
-			name := elems[4 * id + 0].Text()
-			department := elems[4 * id + 1].Text()
-			extension  := elems[4 * id + 2].Text()
-			phone      := elems[4 * id + 3].Text()
+		for id := 0; id < rowCnt; id++ {
+			name := elems[4*id+0].Text()
+			department := elems[4*id+1].Text()
+			extension := elems[4*id+2].Text()
+			phone := elems[4*id+3].Text()
 			fmt.Println(seperator)
 			fmt.Println("Name:\t\t", name)
 			fmt.Println("Department:\t", department)
@@ -87,6 +86,3 @@ func main() {
 
 	app.Run(os.Args)
 }
-
-
-
